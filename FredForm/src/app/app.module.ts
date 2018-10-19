23ae83ba4de5acde341ab260ce4eb096064a921f
icaro martins <icaro@timais.com>
2018-10-19 16:55:17 -0300

    criação da lista de apontamento

--- FredForm/src/app/app.module.ts
+++ FredForm/src/app/app.module.ts
@@ -3,17 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatMenuModule, MatListModule, MatIconModule, MatCardModule, MatGridListModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatMenuModule, MatListModule, MatIconModule, MatCardModule, MatGridListModule, MatTableModule, MatInputModule} from '@angular/material';
 import { InicioComponent } from './inicio/inicio.component';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { LayoutComponent } from './layout/layout.component';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { NgbModule, NgbPaginationModule, NgbAlertModule, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { DragAndDropModule } from 'angular-draggable-droppable';
-import { FlatpickrModule } from 'angularx-flatpickr';
+import { MomentModule } from 'ngx-moment';
 
 @NgModule({
   declarations: [
@@ -25,7 +22,7 @@ import { FlatpickrModule } from 'angularx-flatpickr';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule, 
+    MatButtonModule,
     MatCheckboxModule,
     MatToolbarModule,
     MatSidenavModule,
@@ -35,17 +32,14 @@ import { FlatpickrModule } from 'angularx-flatpickr';
     MatCardModule,
     MatGridListModule,
     LayoutModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory
-    }),
     NgbModule,
     NgbPaginationModule,
     NgbAlertModule,
     FormsModule,
     NgbModalModule,
-    DragAndDropModule,
-    FlatpickrModule.forRoot()
+    MatTableModule,
+    MomentModule,
+    MatInputModule
   ],
   exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
